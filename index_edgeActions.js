15da6c843352af892a9903f0382f84b72f03b204
Andrey <pegas344@gmail.com>
2017-04-10 16:32:33 +0300

    banner
    for show banner

---
+++ index_edgeActions.js
@@ -0,0 +1,35 @@
+/***********************
+* Adobe Edge Animate Composition Actions
+*
+* Edit this file with caution, being careful to preserve 
+* function signatures and comments starting with 'Edge' to maintain the 
+* ability to interact with these actions from within Adobe Edge Animate
+*
+***********************/
+(function($, Edge, compId){
+var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 10302, function(sym, e) {
+         // insert code here
+         sym.play(1);
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${Rectangle}", "click", function(sym, e) {
+         // insert code for mouse click here
+         // Navigate to a new URL in a new window
+         // (replace "_blank" with appropriate target attribute)
+         window.open("http://yama.kiev.ua/", "_blank");
+
+      });
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+})(window.jQuery || AdobeEdge.$, AdobeEdge, "EDGE-52640412");
